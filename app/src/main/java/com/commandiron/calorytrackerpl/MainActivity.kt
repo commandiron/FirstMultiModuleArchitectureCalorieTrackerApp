f3884a42c728c715bffcc4364933e755ad2d1c9e
Emir <commandiron@gmail.com>
2022-05-09 16:38:26 +0300

    Initial commit

---
+++ app/src/main/java/com/commandiron/calorytrackerpl/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.commandiron.calorytrackerpl
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.commandiron.calorytrackerpl.ui.theme.CaloryTrackerPlTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CaloryTrackerPlTheme {
+
+            }
+        }
+    }
+}
